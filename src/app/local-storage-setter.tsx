58184acb8c5d9ee1e1e0545fa2458c2d758b4117
nadering <120453768+nadering@users.noreply.github.com>
2024-11-25 16:40:32 +0900

    Fix: localStorage 예외 처리 추가
    - 추후 업데이트로 Depot 등이 변경되었을 때, 에러가 발생하는 상황을 막기 위해 예외 처리 추가
    - ToDo: 확인 후 설정되어 있는 값은 설정되도록 변경 필요

--- src/app/local-storage-setter.tsx
+++ src/app/local-storage-setter.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import {
   expAtom,
+  makeEmptyDepot,
   selectedOperatorsAtom,
   selectedOperatorsMaterialAtom,
   userDepotAtom,
@@ -11,6 +12,7 @@ import {
   userNeedAtom,
   userNeedInitializedAtom,
 } from "@/store";
+import { EXP } from "@/data/material";
 
 /** localStorage 불러오기 및 저장을 담당하는 클라이언트 레이아웃 컴포넌트 */
 export default function LocalStorageSetter() {
@@ -27,7 +29,9 @@ export default function LocalStorageSetter() {
   const [userDepotInitialized, setUserDepotInitialized] = useAtom(
     userDepotInitializedAtom
   );
-  const [userNeedInitialized, setUserNeedInitialized] = useAtom(userNeedInitializedAtom);
+  const [userNeedInitialized, setUserNeedInitialized] = useAtom(
+    userNeedInitializedAtom
+  );
 
   // 선택된 오퍼레이터
   const [selectedOperators, setSelectedOperators] = useAtom(
@@ -47,21 +51,33 @@ export default function LocalStorageSetter() {
     // 사용자의 현재 재료 보유량
     const userDepotSaved = localStorage.getItem("userDepot");
     if (userDepotSaved) {
-      setUserDepot(JSON.parse(userDepotSaved));
-      setUserDepotInitialized(true);
+      try {
+        setUserDepot(JSON.parse(userDepotSaved));
+        setUserDepotInitialized(true);
+      } catch {
+        setUserDepot(makeEmptyDepot());
+      }
     }
 
     // 사용자의 필요 재료
     const userNeedSaved = localStorage.getItem("userNeed");
     if (userNeedSaved) {
-      setUserNeed(JSON.parse(userNeedSaved));
-      setUserNeedInitialized(true);
+      try {
+        setUserNeed(JSON.parse(userNeedSaved));
+        setUserNeedInitialized(true);
+      } catch {
+        setUserNeed(makeEmptyDepot());
+      }
     }
 
     // 오퍼레이터 육성에 필요한 총 경험치
     const expSaved = localStorage.getItem("exp");
     if (expSaved) {
-      setExp(JSON.parse(expSaved));
+      try {
+        setExp(JSON.parse(expSaved));
+      } catch {
+        setExp({ material: EXP, count: 0 });
+      }
     }
 
     // 선택된 오퍼레이터의 육성 재화
@@ -69,13 +85,23 @@ export default function LocalStorageSetter() {
       "selectedOperatorsMaterial"
     );
     if (selectedOperatorsMaterialSaved) {
-      setSelectedOperatorsMaterial(JSON.parse(selectedOperatorsMaterialSaved));
+      try {
+        setSelectedOperatorsMaterial(
+          JSON.parse(selectedOperatorsMaterialSaved)
+        );
+      } catch {
+        setSelectedOperatorsMaterial([]);
+      }
     }
 
     // 선택된 오퍼레이터
     const selectedOperatorsSaved = localStorage.getItem("selectedOperators");
     if (selectedOperatorsSaved) {
-      setSelectedOperators(JSON.parse(selectedOperatorsSaved));
+      try {
+        setSelectedOperators(JSON.parse(selectedOperatorsSaved));
+      } catch {
+        setSelectedOperators([]);
+      }
     }
 
     // 데이터를 가져왔음을 알림
@@ -115,4 +141,4 @@ export default function LocalStorageSetter() {
   }, [dataFetched, selectedOperators, selectedOperatorsMaterial]);
 
   return <></>;
-};
+}
