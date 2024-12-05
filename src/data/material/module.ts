adebfbba836c5bc8a1304ec83821cb3d79670933
nadering <120453768+nadering@users.noreply.github.com>
2024-12-06 01:57:48 +0900

    Fix: 데이터 메모리, 리더기 이름 수정
    - 모듈 재료인 데이터 메모리, 데이터 리더기의 이름이 반대로 작성되어 있던 현상 수정

--- src/data/material/module.ts
+++ src/data/material/module.ts
@@ -12,20 +12,20 @@ export const moduleDataBlock: Material = {
   tier: 5,
 };
 
-/** (4T) 데이터 리더기 */
+/** (4T) 데이터 메모리 */
 export const dataSupplementStick: Material = {
   id: "mod_update_token_1",
-  name: "데이터 리더기",
+  name: "데이터 메모리",
   objectName: "dataSupplementStick",
   imageFilename: "data-supplement-stick",
   type: "Module",
   tier: 4,
 };
 
-/** (5T) 데이터 메모리 */
+/** (5T) 데이터 리더기 */
 export const dataSupplementInstrument: Material = {
   id: "mod_update_token_2",
-  name: "데이터 메모리",
+  name: "데이터 리더기",
   objectName: "dataSupplementInstrument",
   imageFilename: "data-supplement-instrument",
   type: "Module",
